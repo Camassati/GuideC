514948e816a4e87808509295ca052d77e6797b8a
Gustavo Martins <gfirminocruz@gmail.com>
2023-12-28 21:45:28 -0300

    Add list to my Guide

--- AprendizadoGeral/AprendizadoGeral/Listas.cs
+++ AprendizadoGeral/AprendizadoGeral/Listas.cs
@@ -10,22 +10,66 @@ namespace AprendizadoGeral
     {
         public void List()
         {
-            /*Lista é uma estrutura de dados:
-             * Homogênea(dados do mesmo tipo)
-             * Ordenada (Elementos acessados por meio de posições)
-             * Inicia vazia e seus elementos sõa alocados sob demanda
-             * Cada elemento ocupa um nó (ou nodo) da lista
-             * -------------------------------------------------------------
-             * Classe: List
-             * Namespace: System.Collections.Generic
-             * -------------------------------------------------------------
-             * Vantagens:
-             * Tamanho variavel
-             * Facilidade para se realizar inserções e deleções
-             * -------------------------------------------------------------
-             * Desvantagens:
-             * Acesso seuquencial aos elementos* ela pode ser otimizada 
-             */
+
+            List<string> list = new List<string>();
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+
+            foreach (string ob in list)
+            {
+                Console.WriteLine(ob);
+            }
+            list.Insert(2, "Marco");
+            Console.WriteLine("-----------------------------");
+            foreach (string ob in list)
+            {
+                Console.WriteLine(ob);
+            }
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine("List count " + list.Count);
+            Console.WriteLine("-----------------------------");
+
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("Fist 'A': " + s1);
+            string s2 = list.Last(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s2);
+            Console.WriteLine("-----------------------------");
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("Fist position 'A': " + pos1);
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Fist position 'A': " + pos2);
+            Console.WriteLine("-----------------------------");
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("Nova lista com strings de tamanho == 5");
+            foreach (string obj in list2)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("-----------------------------");
+            list.Remove("Alex");
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("-----------------------------");
+            list.RemoveAll(x => x[0] == 'M');
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("-----------------------------");
+            list.RemoveAt(list.Count - 1/*Pode ser simplismente o numero"2 ou 1 ou 7"*/);
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            list.RemoveRange(2, 2);
 
         }
     }
